be2544901352ea684e59bdb0f3bf551d4542f835
jaewoo <jaewoo.han@neurophet.com>
2021-04-21 08:28:31 +0900

    feat: 내용 수정 test

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,11 +1,12 @@
-import React, { ReactElement } from 'react';
+import React, {ReactElement} from 'react';
 import {Button, Input} from "@aqua/components";
 
 const App = (): ReactElement => {
   return (
     <div className="App">
-      <Button />
-      <Input />
+      <Button/>
+      <Input/>
+      <div>내용 수정</div>
     </div>
   );
 };
